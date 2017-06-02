da1cb467f9ecdffcb2d4fa4576dd611c5f19f8f6
zhanglong <jianglong161@163.com>
2017-06-02 22:48:49 +0800

    用打游戏的例子表示代理

---
+++ ProxyPattern/ProxyPattern/src/com/expale/IGamePlayer.java
@@ -0,0 +1,10 @@
+package com.expale;
+
+public interface IGamePlayer {
+	//µÇÂ¼ÓÎÏ·
+	public void login(String usrr,String password);
+	//É±¹Ö
+	public void killBoss();
+	//Éý¼¶
+	public void upgrade();
+}
