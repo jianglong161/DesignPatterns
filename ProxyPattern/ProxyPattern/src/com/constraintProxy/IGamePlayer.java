99d4c610a26f7dc15e4b6d0f0201c1abe16fe656
zhanglong <jianglong161@163.com>
2017-06-03 14:20:05 +0800

    强制代理-接口类

---
+++ ProxyPattern/ProxyPattern/src/com/constraintProxy/IGamePlayer.java
@@ -0,0 +1,12 @@
+package com.constraintProxy;
+
+public interface IGamePlayer {
+	//登录游戏
+	public void login(String username,String password);
+	//杀怪
+	public void killBoss();
+	//升级
+	public void upgrade();
+	//每个人都一颗找一下自己代理
+	public IGamePlayer getProxy();
+}
