c73f16e5ae3a383687693480a340b89652f4ce56
zhanglong <jianglong161@163.com>
2017-06-11 10:49:53 +0800

    单利模式举例-臣子类

---
+++ SingletonPattern/SingletonPattern/src/com/example/Minister.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.Iterator;
+
+/**
+ * ³¼×ÓÀà
+ * @author Long
+ *
+ */
+public class Minister {
+	public static void main(String[] args) {
+		for(int day = 0; day < 3; day ++){
+			Emperor emperor = Emperor.getInstance();
+			emperor.say();
+		}
+	}
+}
