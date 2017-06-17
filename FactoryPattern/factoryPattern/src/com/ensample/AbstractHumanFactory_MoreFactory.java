b89829bcee7281871b60cc767f0bf2b2b9248408
zhanglong <jianglong161@163.com>
2017-06-17 16:03:13 +0800

    多工厂模式

---
+++ FactoryPattern/factoryPattern/src/com/ensample/AbstractHumanFactory_MoreFactory.java
@@ -0,0 +1,9 @@
+package com.ensample;
+/**
+ * 多工厂模式的抽象工厂类
+ * @author Still2Almost
+ *
+ */
+public  abstract class AbstractHumanFactory_MoreFactory {
+	public abstract Human creatman();
+}
---
+++ FactoryPattern/factoryPattern/src/com/ensample/BlackHumanFactory.java
@@ -0,0 +1,11 @@
+package com.ensample;
+
+public class BlackHumanFactory extends AbstractHumanFactory_MoreFactory{
+
+	@Override
+	public Human creatman() {
+		// TODO Auto-generated method stub
+		return new BlackHuman();
+	}
+
+}
