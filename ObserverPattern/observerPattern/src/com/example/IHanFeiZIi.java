2c801d8b6af25130ee2bf27b3b853d7b9d227264
zhanglong <jianglong161@163.com>
2017-06-24 15:50:01 +0800

    被观察的接口-韩非子

---
+++ ObserverPattern/observerPattern/src/com/example/IHanFeiZIi.java
@@ -0,0 +1,12 @@
+package com.example;
+/**
+ * 被观察者接口
+ * @author Still2Almost
+ *
+ */
+public interface IHanFeiZIi {
+	//韩非子也是人，也要吃饭
+	public void haveBreakfast();
+	//韩非子也是人， 是人就要娱乐活动
+	public void haveFun();
+}
