6ac2f78ac8c8231f28dfc1fbdaa4da52b64b2f88
zhanglong <jianglong161@163.com>
2017-07-01 15:16:21 +0800

    源角色

---
+++ AdapterPattern/adapterPattern/src/com/common/Adapee.java
@@ -0,0 +1,12 @@
+package com.common;
+/**
+ * 源角色
+ * @author Still2Almost
+ *
+ */
+public class Adapee {
+	//原有的业务的逻辑
+	public void doSomething(){
+		System.out.println("Im kind of busy ,kean me alone");
+	}
+}
