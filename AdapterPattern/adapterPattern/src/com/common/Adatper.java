1a42a1d644ba49337f5a70ba9f6c3226adda6707
zhanglong <jianglong161@163.com>
2017-07-01 15:18:54 +0800

    适配器角色

---
+++ AdapterPattern/adapterPattern/src/com/common/Adatper.java
@@ -0,0 +1,15 @@
+package com.common;
+/**
+ * ÊÊÅäÆ÷½ÇÉ«
+ * @author Still2Almost
+ *
+ */
+public class Adatper extends Adapee implements Target{
+
+	@Override
+	public void request() {
+		super.doSomething();
+		
+	}
+	
+}
