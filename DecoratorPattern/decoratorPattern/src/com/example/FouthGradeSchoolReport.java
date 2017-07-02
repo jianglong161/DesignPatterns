309314613abfdf085688d67cd16e24cd183d91f1
zhanglong <jianglong161@163.com>
2017-07-02 15:57:08 +0800

    四年级成绩单

---
+++ DecoratorPattern/decoratorPattern/src/com/example/FouthGradeSchoolReport.java
@@ -0,0 +1,28 @@
+package com.example;
+/**
+ * 四年级成绩单
+ * @author Long
+ *
+ */
+public class FouthGradeSchoolReport extends SchoolReport{
+
+	@Override
+	public void report() {
+		System.out.println("尊敬的XXXX家长");
+		System.out.println(".......");
+		System.out.println("语文 63   数学 65 体育98 自然 63");
+		System.out.println(".....");
+		System.out.println("                  家长签字：");
+	}
+
+	public void sign(String name) {
+		System.out.println("家长签名：" + name);
+	}
+
+	@Override
+	public void sign() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
